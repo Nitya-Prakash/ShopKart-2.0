bfacd40a2c755ff6c9f752795ee002d8b936097b
Nitya-Prakash <nityaprakashsahoo677@gmail.com>
2024-10-18 21:16:40 +0530

    Updating ProductDetails

--- src/Component/ProductDetails.jsx
+++ src/Component/ProductDetails.jsx
@@ -3,17 +3,6 @@ import { Link } from 'react-router-dom'
 
 const ProductDetails = () => {
     return (
-        // <div className='w-[70%] h-full bg-red-100 m-auto p-28 flex items-center'>
-        //     <img className='h-[90%] mr-16' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
-        //     <div className="content">
-        //         <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, eius?</h1>
-        //         <h2>Men's Clothing</h2>
-        //         <h2>$109.95</h2>
-        //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo libero nulla deserunt cumque error nam tempore aut tempora sequi commodi.</p>
-        //         <Link>Edit</Link>
-        //         <Link>Delete</Link>
-        //     </div>
-        // </div>
         <div className='w-[70%] h-auto m-auto px-10 py-5 flex items-center'>
             <img className='h-[90%] w-[40%] object-contain mr-16' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
             <div className="content flex flex-col gap-2">
