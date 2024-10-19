3730f9f56e89039edfb4f1549aa733c045d4c6e5
Nitya-Prakash <nityaprakashsahoo677@gmail.com>
2024-10-19 21:04:14 +0530

    Linking the api to my project and showcasing all the products on the home page

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from './Navbar'
 import { Link } from 'react-router-dom'
+import { ProductContext } from '../utils/Context'
+import Loading from './Loading'
 
 const Home = () => {
-    return (
+    const [products] = useContext(ProductContext)
+    // console.log(products)
+
+    return products ? (
         <>
             <Navbar />
             <div className='w-[80%] h-full p-2 flex flex-wrap overflow-x-hidden overflow-y-auto'>
-                <Link to="/ProductDetails/1" className='w-[23%] h-[40vh] mb-3 mr-3 card p-3 border shadow rounded flex flex-col items-center justify-center cursor-pointer'>
-                    <div className='w-full h-[80%] mb-3 bg-contain bg-no-repeat bg-center hover:scale-105' style={{ backgroundImage: "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)" }}></div>
-                    <h2 className='text-center text-base font-medium'>Fjallraven-Foldsack No.1 Backpack, Fits 15 Laptops</h2>
-                </Link>
+
+                {products.map((item, index) =>
+
+                    <Link key={index} to={`/ProductDetails/${item.id}`} className='w-[23%] h-[40vh] mb-3 mr-3 card p-3 border shadow rounded flex flex-col items-center justify-center cursor-pointer'>
+                        <div className='w-full h-[80%] mb-3 bg-contain bg-no-repeat bg-center hover:scale-105' style={{ backgroundImage: `url(${item.image})` }}></div>
+                        <h2 className='text-center text-base leading-4 font-medium'>{item.title}</h2>
+                    </Link>
+                )}
+
+
             </div>
         </>
-    )
+    ) : <Loading />
 }
 
 export default Home
--- src/Component/ProductDetails.jsx
+++ src/Component/ProductDetails.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from '../utils/Axios'
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Loading from './Loading'
 
 const ProductDetails = () => {
-    return (
+    const [singleProduct, setSingleProduct] = useState(null)
+
+    const { id } = useParams();
+    // console.log(id)
+
+    const getSingleProducts = async () => {
+        try {
+            const { data } = await axios.get(`/products/${id}`)
+            setSingleProduct(data)
+            // console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getSingleProducts();
+    }, [])
+
+    return singleProduct ? (
         <div className='w-[70%] h-auto m-auto px-10 py-5 flex items-center'>
-            <img className='h-[90%] w-[40%] object-contain mr-16' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
+            <img className='h-[90%] w-[40%] object-contain mr-16' src={`${singleProduct.image}`} alt="" />
             <div className="content flex flex-col gap-2">
-                <h1 className='text-4xl font-semibold'>Fjallraven-Foldsack No.1 Backpack, Fits 15 Laptops</h1>
-                <h2 className='text-zinc-600 font-medium text-lg'>Men's Clothing</h2>
-                <h2 className='text-red-400 font-medium text-lg'>$109.95</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo libero nulla deserunt cumque error nam tempore aut tempora sequi commodi.</p>
+                <h1 className='text-4xl font-semibold'>{singleProduct.title}</h1>
+                <h2 className='text-zinc-600 font-medium text-lg'>{singleProduct.category}</h2>
+                <h2 className='text-red-400 font-medium text-lg'>&#36; {singleProduct.price}</h2>
+                <p>{singleProduct.description}</p>
                 <div className='mt-5 flex gap-5'>
                     <Link className='px-5 py-2 border border-blue-400 rounded-md text-blue-400'>Edit</Link>
                     <Link className='px-5 py-2 border border-red-400 rounded-md text-red-400'>Delete</Link>
                 </div>
             </div>
         </div>
-    )
+    ) : <Loading />
 }
 
 export default ProductDetails
